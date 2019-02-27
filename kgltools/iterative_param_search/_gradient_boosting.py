8be1aa43d7e453a57936ecc5ca0bf8ff81a66383
aborisihin <a.borisihin@gmail.com>
2019-02-27 16:35:14 +0300

    ips - wip

--- kgltools/iterative_param_search/_gradient_boosting.py
+++ kgltools/iterative_param_search/_gradient_boosting.py
@@ -32,26 +32,23 @@ class GBoostNTreesSearcher(IPSStageBase):
             self.logger.log('Wrong param_grid argument!')
             return dict()
 
-        max_n_estimators = list(self.param_grid.values())[0]
         n_estimators_param_name = list(self.param_grid.keys())[0]
+        max_n_estimators = list(self.param_grid.values())[0]
         fitted_params = {n_estimators_param_name: 0}
-
-        if max_n_estimators <= 0:
-            self.logger.log('Wrong value in param_grid!')
-            return fitted_params
 
         self.logger.start_timer()
 
         train_mean, train_std, test_mean, test_std = 0.0, 0.0, 0.0, 0.0
 
+        # xgboost
         if self.parent.estimator_class in (XGBClassifier, XGBRegressor):
             xgb_params = self.parent.estimator_class(**params).get_xgb_params()
             xgd_data = xgb.DMatrix(self.parent.X.values, label=self.parent.y.values)
             cv_result = xgb.cv(params=xgb_params,
                                dtrain=xgd_data,
+                               metrics=self.parent.estimator_metrics,
                                num_boost_round=max_n_estimators,
                                nfold=self.parent.n_folds,
-                               metrics=self.parent.metrics,
                                early_stopping_rounds=self.early_stopping_rounds,
                                stratified=self.parent.stratified,
                                seed=self.parent.random_state,
@@ -67,6 +64,6 @@ class GBoostNTreesSearcher(IPSStageBase):
             self.logger.log_timer()
             self.logger.log('train: {:0.5f} (std={:0.5f})'.format(train_mean, train_std))
             self.logger.log('test: {:0.5f} (std={:0.5f})'.format(test_mean, test_std))
-            self.logger.log('{} = {}'.format(n_estimators_param_name, fitted_params[n_estimators_param_name]))
+            self.logger.log(fitted_params)
 
         return fitted_params
--- kgltools/iterative_param_search/_pipeline.py
+++ kgltools/iterative_param_search/_pipeline.py
@@ -2,64 +2,85 @@
 Contains tools for iterative search for best parameters of model
 """
 
+import os
+import json
 from typing import Generic, Union, List, Tuple
+from collections.abc import Iterable
 from abc import ABCMeta, abstractmethod
 
 import pandas as pd
+from sklearn.model_selection import GridSearchCV
 
 from ..logger import Logger
 
 
-__all__ = ['IPSPipeline', 'IPSStageBase']
+__all__ = ['IPSPipeline', 'IPSStageBase', 'IPSConstantSetter', 'IPSGridSearcher', 'IPSGridFineSearcher']
 
 
 class IPSPipeline(object):
+    # from context:
+    # data, random_state, metrics_mapping, estimator_parameters_limits
 
     def __init__(self,
                  estimator_class: Generic,
                  X: pd.DataFrame,
                  y: Union[pd.DataFrame, pd.Series],
                  metrics: str,
-                 n_folds: int = 3,
+                 n_folds: int = 5,
                  stratified: bool = True,
                  shuffle: bool = True,
                  base_params: dict = {},
                  random_state: int = 0,
+                 n_jobs: int = -1,
                  verbose: bool = True) -> None:
         self.estimator_class = estimator_class
         self.X = X
         self.y = y
         self.metrics = metrics
         self.n_folds = n_folds
-        self.metrics = metrics
         self.stratified = stratified
         self.shuffle = shuffle
         self.base_params = base_params
         self.random_state = random_state
+        self.n_jobs = n_jobs
         self.verbose = verbose
 
         self.stages = []
-        self.best_params = base_params
+        self.best_params = None
         self.logger = Logger(nesting_level=0)
 
-    def add_stages(self, stage_descriptors: List[Tuple[str, object, dict]]) -> None:
-        for stage_name, stage_object, stage_grid in stage_descriptors:
-            self.add_stage(stage_name, stage_object, stage_grid)
+        ###############################################
+        cur_path = os.path.dirname(os.path.abspath(__file__))
+        settings_dir = os.path.abspath(os.path.join(cur_path, '../../settings'))
 
-    def add_stage(self, stage_name: str, stage_object: object, stage_grid: dict) -> None:
-        # if IPSStageBase not in inspect.getmro(stage_type):
-        if not isinstance(stage_object, IPSStageBase):
-            self.logger.log('Can\'t add stage: {}. Must be an instance of IPSStageBase.'.format(stage_name))
-            return
+        # metrics_mapping
+        mm_path = os.path.join(settings_dir, 'metrics_mapping.json')
+        if not os.path.exists(mm_path):
+            print('Settings file metrics_mapping.json is not exist!')
+            self.metrics_mapping = None
+        else:
+            with open(mm_path, 'r') as mm_file:
+                self.metrics_mapping = json.load(mm_file)
 
-        stage_object.set_parent_pipeline(self)
-        stage_object.set_param_grid(stage_grid)
-        self.stages.append((stage_name, stage_object))
+        # estimator_parameter_limits
+        epl_path = os.path.join(settings_dir, 'estimator_parameter_limits.json')
+        if not os.path.exists(epl_path):
+            print('Settings file estimator_parameter_limits.json is not exist!')
+            self.estimator_parameter_limits = None
+        else:
+            with open(epl_path, 'r') as epl_file:
+                self.estimator_parameter_limits = json.load(epl_file)
+        ###############################################
+
+        self.estimator_classname = '{}.{}'.format(estimator_class.__module__, estimator_class.__name__)
+        self.estimator_metrics = self.metrics_mapping[metrics][self.estimator_classname]
 
     def fit(self) -> None:
         if self.verbose:
-            self.logger.log('IPSPipeline ({})'.format(type(self.estimator_class()).__name__))
+            self.logger.log('IPSPipeline ({})'.format(self.estimator_classname))
             self.logger.start_timer()
+
+        self.best_params = self.base_params
 
         for stage_name, stage in self.stages:
             if self.verbose:
@@ -69,6 +90,44 @@ class IPSPipeline(object):
 
         if self.verbose:
             self.logger.log_timer()
+
+    def add_stages(self, stage_descriptors: List[Tuple[object, dict]]) -> None:
+        for stage_object, stage_grid in stage_descriptors:
+            self.add_stage(stage_object, stage_grid)
+
+    def add_stage(self, stage_object: object, stage_grid: dict) -> None:
+        stage_name = '{}({})'.format(type(stage_object).__name__, ','.join(list(stage_grid.keys())))
+
+        if not isinstance(stage_object, IPSStageBase):
+            self.logger.log('Can\'t add stage: {}. Must be an instance of IPSStageBase.'.format(stage_name))
+            return
+
+        grid_errors = self.check_parameter_limits(stage_grid)
+        if len(grid_errors) > 0:
+            self.logger.log('Can\'t add stage: <{}>. Wrong values for {}.'.format(stage_name, grid_errors))
+            return
+
+        stage_object.set_parent_pipeline(self)
+        stage_object.set_param_grid(stage_grid)
+        self.stages.append((stage_name, stage_object))
+
+    def check_parameter_limits(self, param_grid: dict) -> List[str]:
+        if self.estimator_classname not in self.estimator_parameter_limits:
+            return [] # nothing to check
+        limits = self.estimator_parameter_limits[self.estimator_classname]
+        errors = []
+        for param, values_list in param_grid.items():
+            if param in limits:
+                if not isinstance(values_list, list):
+                    if not isinstance(values_list, Iterable):
+                        values_list = [values_list]
+                    else:
+                        values_list = list(values_list)
+                param_min, param_max = limits[param]
+                for val in values_list:
+                    if (param_min and (val < param_min)) or (param_max and (val > param_max)):
+                        errors.append(param)
+        return errors
 
 
 class IPSStageBase(object):
@@ -89,13 +148,49 @@ class IPSStageBase(object):
         pass
 
 
+class IPSConstantSetter(IPSStageBase):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)   
+
+    def process(self, params: dict = {}) -> dict:
+        if self.parent.verbose:
+            self.logger.log(self.param_grid)
+        return self.param_grid
+
+
 class IPSGridSearcher(IPSStageBase):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
     def process(self, params: dict = {}) -> dict:
-        pass
+        self.logger.start_timer()
+
+        train_mean, train_std, test_mean, test_std = 0.0, 0.0, 0.0, 0.0
+
+        g_search = GridSearchCV(
+            estimator=self.parent.estimator_class(**params),
+            param_grid=self.param_grid,
+            return_train_score=True,
+            cv=self.parent.n_folds,
+            scoring=self.parent.metrics,
+            n_jobs=self.parent.n_jobs,
+            verbose=False)
+        g_search.fit(self.parent.X, self.parent.y)
+
+        train_mean = g_search.cv_results_['mean_train_score'][g_search.best_index_]
+        train_std = g_search.cv_results_['std_train_score'][g_search.best_index_]
+        test_mean = g_search.cv_results_['mean_test_score'][g_search.best_index_]
+        test_std = g_search.cv_results_['std_test_score'][g_search.best_index_]
+
+        if self.parent.verbose:
+            self.logger.log_timer()
+            self.logger.log('train: {:0.5f} (std={:0.5f})'.format(train_mean, train_std))
+            self.logger.log('test: {:0.5f} (std={:0.5f})'.format(test_mean, test_std))
+            self.logger.log(g_search.best_params_)
+
+        return g_search.best_params_
 
 
 class IPSGridFineSearcher(IPSStageBase):
@@ -104,4 +199,6 @@ class IPSGridFineSearcher(IPSStageBase):
         super().__init__(*args, **kwargs)
 
     def process(self, params: dict = {}) -> dict:
+        # import math
+        # math.floor(math.log(1, 10))
         pass
